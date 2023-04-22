8fc62816468cfc20c856310333cca4b3694ea478
k2esone <dkowalczyk.lwc@gmail.com>
2023-04-22 10:14:16 +0200

    first commit

---
+++ src/test/java/pl/sdacademy/pokemonapi/PokemonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.pokemonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
