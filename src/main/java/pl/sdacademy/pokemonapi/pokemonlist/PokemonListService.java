ec0f82234174b6a89724cb01ed34c73d17e9cff5
k2esone <dkowalczyk.lwc@gmail.com>
2023-04-23 13:25:06 +0200

    feature(paa): use db cache when fetching pokemon list

--- src/main/java/pl/sdacademy/pokemonapi/pokemonlist/PokemonListService.java
+++ src/main/java/pl/sdacademy/pokemonapi/pokemonlist/PokemonListService.java
@@ -8,32 +8,21 @@ import java.util.List;
 @Service
 class PokemonListService {
 
-    private final PokeApiListNetworkRepository pokeApiListNetworkRepository;
+    private final FetchPokemonListUseCase pokemonListUseCase;
     private final PokemonListItemRepository pokemonListItemRepository;
-    private final PokemonListItemMapper pokemonListItemMapper;
 
-    PokemonListService(
-            PokeApiListNetworkRepository pokeApiListNetworkRepository,
-            PokemonListItemRepository pokemonListItemRepository,
-            PokemonListItemMapper pokemonListItemMapper
-    ) {
-        this.pokeApiListNetworkRepository = pokeApiListNetworkRepository;
+    public PokemonListService(
+            FetchPokemonListUseCase pokemonListUseCase,
+            PokemonListItemRepository pokemonListItemRepository) {
+        this.pokemonListUseCase = pokemonListUseCase;
         this.pokemonListItemRepository = pokemonListItemRepository;
-        this.pokemonListItemMapper = pokemonListItemMapper;
     }
 
     List<PokemonListItemEntity> getPokemonItemList() {
-        List<PokeApiListItemResult> results = new ArrayList<>();
-        PokeApiListResult result;
-        int limit = 100;
-        int offset = 0;
-        do {
-            result = pokeApiListNetworkRepository.fetchPokemonListResult(limit, offset);
-            results.addAll(result.getResults());
-            offset += limit;
-        } while (result.getNext() != null);
-        List<PokemonListItemEntity> pokemonListItemEntities =
-                results.stream().map(pokemonListItemMapper::toEntity).toList();
-        return pokemonListItemRepository.saveAll(pokemonListItemEntities);
+        if (pokemonListItemRepository.count() != 0) {
+            return pokemonListItemRepository.findAll();
+        } else {
+            return pokemonListUseCase.execute();
+        }
     }
 }
